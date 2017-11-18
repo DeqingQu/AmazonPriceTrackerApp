b844bca1b23a47b546df41779f03ed5d65a2772f
DeqingQu <qu.deqing@gmail.com>
2017-11-17 22:01:50 -0800

    show all urls in table

--- AmazonPriceTrackerApp/ViewController.m
+++ AmazonPriceTrackerApp/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 #import "AFNetworking.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property NSMutableArray *commodity_urls;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -18,25 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _commodity_urls = [[NSMutableArray alloc] initWithCapacity:0];
     
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-    
-
-    NSString *URLString = @"http://192.168.0.40:3000/api/urls";
-    NSDictionary *parameters = @{};
-
-    NSURLRequest *request = [[AFJSONRequestSerializer serializer] requestWithMethod:@"GET" URLString:URLString parameters:parameters error:nil];
-
-    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
-        if (error) {
-            NSLog(@"Error: %@", error);
-        } else {
-            NSLog(@"%@ %@", response, responseObject);
-        }
-    }];
-    [dataTask resume];
-
+    [self callAPI];
 }
 
 
@@ -45,5 +32,54 @@
     // Dispose of any resources that can be recreated.
 }
 
+//  private methods
+- (void)callAPI {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSString *URLString = @"http://192.168.0.40:3000/api/urls";
+    NSDictionary *parameters = @{};
+    
+    NSURLRequest *request = [[AFJSONRequestSerializer serializer] requestWithMethod:@"GET" URLString:URLString parameters:parameters error:nil];
+    
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            NSLog(@"%@", response);
+            NSLog(@"%@", [responseObject objectForKey:@"data"]);
+            NSArray *data = [responseObject objectForKey:@"data"];
+            for(int i=0; i<[data count]; i++) {
+                [_commodity_urls addObject:[data objectAtIndex:i]];
+            }
+            [_tableView reloadData];
+        }
+    }];
+    [dataTask resume];
+}
 
+
+//  UITableViewDelegate / UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_commodity_urls count];
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 50;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString * cellID = [NSString stringWithFormat:@"cell_sec_%ld", (long)indexPath.section ];
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    NSInteger c_id = [[[_commodity_urls objectAtIndex:(long)indexPath.row] objectForKey:@"c_id"] integerValue];
+    NSString *c_url = [[_commodity_urls objectAtIndex:(long)indexPath.row] objectForKey:@"c_url"];
+    cell.textLabel.text = [NSString stringWithFormat:@"ID - %ld", c_id];
+    cell.detailTextLabel.text = c_url;
+    return cell;
+}
 @end
