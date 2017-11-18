b01289248f007e51fec33cb730920464668e2316
DeqingQu <qu.deqing@gmail.com>
2017-11-17 21:00:05 -0800

    call api in app

--- AmazonPriceTrackerApp/ViewController.m
+++ AmazonPriceTrackerApp/ViewController.m
@@ -18,6 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+
+    NSString *URLString = @"http://192.168.0.40:3000/api/urls";
+    NSDictionary *parameters = @{};
+
+    NSURLRequest *request = [[AFJSONRequestSerializer serializer] requestWithMethod:@"GET" URLString:URLString parameters:parameters error:nil];
+
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            NSLog(@"%@ %@", response, responseObject);
+        }
+    }];
+    [dataTask resume];
+
 }
 
 
